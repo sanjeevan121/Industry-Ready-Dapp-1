538a6c1ab0f70bb6b44d3154fafd6d8e638dc197
sanjeevan121 <sanjeevan.k.thorat@gmail.com>
2023-01-23 17:10:52 +0530

    added client

---
+++ client/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React,{useState,useEffect} from 'react';
+import Style from "./NavBar.module.css";
+import Image from "next/image";
+import Link from "next/Link";
+import {MDNotifications} from "react-icons/md";
+import {BSSearch} from "react-icons/bs";
+import {cgMenuLeft,cgMenuRight} from "react-icons/cg";
+import {Discover,HelpCenter,Notification,Profile,SideBar} from "./index";
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
---
+++ client/components/Navbar/index.js
@@ -0,0 +1,7 @@
+import Discover from "./Discover/Discover";
+import HelpCenter from "./HelpCenter/HelpCenter";
+import Profile from "./Profile/Profile";
+import Notification from "./Notifications/Notification";
+import SideBar from "./SideBar/SideBar";
+
+export {Discover,HelpCenter,Profile,Notification,SideBar}
